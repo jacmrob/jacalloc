a986b21f2a672f4bc421ba0c0f6dae8c62059ac6
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-12-15 16:54:16 -0500

    add private field to resources

--- app/models.py
+++ app/models.py
@@ -16,19 +16,21 @@ class Resource(BaseModel):
     ip = db.Column(db.String)
     in_use = db.Column(db.Boolean)
     project = db.Column(db.String)
+    private = db.Column(db.Boolean, default=False)
 
-    def __init__(self, name, ip, in_use, project):
+    def __init__(self, name, ip, in_use, project, private=False):
         self.name = name
         self.ip = ip
         self.in_use = in_use
         self.project = project
+        self.private = private
 
     def __repr__(self):
-        return '<id {0}, name {1}, ip {2}, in_use {3}, project {4}>'.format(self.id, self.name, self.ip, self.in_use, self.project)
+        return '<id {0}, name {1}, ip {2}, in_use {3}, project {4}, private {5}>'.format(self.id, self.name, self.ip, self.in_use, self.project, self.private)
 
     def map(self):
         return {"name": self.name,
                 "ip": self.ip,
                 "project": self.project,
-                "in_use": self.in_use}
-
+                "in_use": self.in_use,
+                "private": self.private}
