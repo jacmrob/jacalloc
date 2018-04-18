4efdd21af3bcf049597eca3ba7eb694721472e36
Jackie Roberti <jacqueline.roberti93@gmail.com>
2018-04-18 17:25:46 -0400

    ugh i'm dumb

--- app/test/apitests.py
+++ app/test/apitests.py
@@ -191,7 +191,7 @@ class TestDependentApi(unittest.TestCase):
     def test_update_field_not_allowed(self):
         resp = requests.post(self.base_url_resources + "/" + self.resource_name, headers=self.headers, data=json.dumps({"usable": False}))
         self.assertEqual(resp.status_code, HTTP_OK)
-        resp2 = requests.post(self.base_url_resources + "/" + self.resource_name, headers=self.headers, data={"in_use": True})
+        resp2 = requests.post(self.base_url_resources + "/" + self.resource_name, headers=self.headers, data=json.dumps({"in_use": True}))
         self.assertEqual(resp2.status_code, HTTP_METHOD_NOT_ALLOWED)
 
     # POST /resources/allocate
